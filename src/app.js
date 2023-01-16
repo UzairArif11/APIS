f11cdf818e64c0a9be73f05e8f07c2b956d3fbdd
uzair <uzair.shayansolutions@gmail.com>
2023-01-16 19:12:28 +0500

    get post delete

--- src/app.js
+++ src/app.js
@@ -7,17 +7,53 @@ const port = process.env.Port || 8000;
 app.use(express.json()); //required to get json data as output
 
 //create a new students
-app.post("/students", (req, res) => {
-  console.log(req.body);
-  const user = new Student(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+app.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+//read data of registered student
+app.get("/students", async (req, res) => {
+  try {
+    const studentsData = await Student.find();
+    res.status(201).send(studentsData);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// get the individual student data using id
+
+app.get("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const studentData = await Student.findById(_id);
+    res.status(201).send(studentData);
+    if (!req.params.id) {
+      return res.status(404).send();
+    }
+    res.status(200).send(error);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// delete student by its id
+
+app.delete("/students/:id", async (req, res) => {
+  try {
+    const deleteStudent = await Student.findByIdAndDelete(req.params.id);
+    if (!req.params.id) {
+      return res.status(404).send();
+    }
+    res.status(200).send(error);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(port, () => {
